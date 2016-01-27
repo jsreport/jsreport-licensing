9d3da50a0ff9262772da9520a6e99146a8068463
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-01-27 12:33:47 +0100

    add licensing information to status bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,31 @@
 define(["app", "marionette", "backbone", "jquery"],
     function (app, Marionette, Backbone, $) {
-        
+
+        app.on("user-info-render", function (context) {
+            context.result = "<li id='licenseInformationHolder'></li>" + context.result;
+        });
+
         app.onStartListeners.add(function(cb) {
             cb();
 
-            if (app.settings.data.license && app.settings.data.license.value)
+            if (app.settings.data.license && app.settings.data.license.value) {
+                $('#licenseInformationHolder').html("<a target='_blank' class='btn-info' style='padding-right:22px' href='http://jsreport.net/buy'>ENTERPRISE LICENSE</a>")
                 return;
+            }
 
             setTimeout(function () {
 
                 $.getJSON("odata/templates/$count", function(data) {
                     if (data.value > 5) {
+                        $('#licenseInformationHolder').html("<a target='_blank' style='padding-right:22px' class='btn-warning' href='http://jsreport.net/buy'>UNLICENSED / BUY</a>")
                         $.dialog({
                             hideSubmit: true,
                             header: "<span class='text text-danger'>Free license exceeded</span>",
                             content: "<p>Free license is limited to maximum 5 templates. Please buy the enterprise license before you continue.</p>" +
                             "<p>The instructions for buying enterprise license can be found <a href='http://jsreport.net/buy' target='_blank'>here</a>.</p>"
                         });
+                    } else {
+                        $('#licenseInformationHolder').html("<a target='_blank' style='padding-right:22px' class='btn-success' href='http://jsreport.net/buy'>FREE LICENSE</a>")
                     }
                 });
             }, 1000);
