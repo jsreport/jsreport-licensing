9b61743520284068b8570e7eb5e9a4814c755c65
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-08-09 17:14:39 +0200

    rework licensing to store offline information in license.json and provide more information to the user

--- studio/main_dev.js
+++ studio/main_dev.js
@@ -1,6 +1,42 @@
 import Studio from 'jsreport-studio'
 
+function renderLicenseType (licensingInfo) {
+  if (licensingInfo.unreachable) {
+    return <p>
+        The licensing server was not reachable during the instance startup. The instance now runs in the enterprise mode and the license validation will be performed again during the next restart.
+      </p>
+  }
+
+  if (licensingInfo.type === 'subscription') {
+    if (licensingInfo.pendingExpiration === true) {
+      return <p>
+        The subscription is no longer active probably due to failed payment or cancellation. The subscription can be used for maximum one month in inactive state.
+        Please verify the state of your subscription in the <a href='https://gumroad.com/library' target='_blank'>gumroad library</a>.
+      </p>
+    }
+
+    return <p>The subscription renewal is planned on {licensingInfo.expiresOn.toLocaleDateString()} and the license will be again validated afterwards.
+      You can find further information about the particular subscription charges in <a href='https://gumroad.com/library' target='_blank'>gumroad library</a>.
+    </p>
+  }
+
+  if (licensingInfo.type === 'perpetual') {
+    return <p>Perpetual license is validated for the version {licensingInfo.validatedForVersion} with free upgrades to the versions released before {licensingInfo.expiresOn.toLocaleDateString()}.
+      The license will be remotely validated again if the instance is upgraded to a different version.</p>
+  }
+
+  if (licensingInfo.type === 'trial') {
+    return <p>The trial license expires on {licensingInfo.expiresOn.toLocaleDateString()}.
+      You will need to purchase enterprise license to be able to store more than 5 templates afterwards.</p>
+  }
+
+  if (licensingInfo.type === 'free') {
+    return <p>You can use up to 5 templates for free.</p>
+  }
+}
+
 Studio.readyListeners.push(async () => {
+  var licensingInfo = Studio.extensions['licensing'].options
   const trialModal = () => Studio.openModal(() => <div><p>Free license is limited to maximum 5 templates.
     Your jsreport instance is now running in one month trial. Please buy
     the enterprise license if you want to continue using jsreport after the trial expires.
@@ -11,23 +47,41 @@ Studio.readyListeners.push(async () => {
     </p>
   </div>)
 
-  if (Studio.extensions['licensing'].options.license === 'trial' && Studio.getAllEntities().filter((e) => e.__entitySet === 'templates' && !e.__isNew).length > 5) {
+  const licenseInfoModal = () => Studio.openModal(() => <div>
+    <h2>{licensingInfo.license} license</h2>
+    {renderLicenseType(licensingInfo)}
+    <p>More information about licensing and pricing can be found <a href='http://jsreport.net/buy' target='_blank'>here</a>.</p>
+  </div>)
+
+  const pendingExpirationModal = () => Studio.openModal(() => <div>
+    <h2>subscription has expired</h2>
+    <p>
+      The subscription is no longer active probably due to failed payment or cancellation. The subscription can be used for maximum one month in inactive state.
+      Please verify the state of your subscription in the <a href='https://gumroad.com/library' target='_blank'>gumroad library</a>.
+    </p>
+  </div>)
+
+  if (licensingInfo.type === 'trial' && Studio.getAllEntities().filter((e) => e.__entitySet === 'templates' && !e.__isNew).length > 5) {
     trialModal()
   }
 
-  if (Studio.extensions['licensing'].options.license === 'free') {
+  if (licensingInfo.type === 'subscription' && licensingInfo.pendingExpiration === true) {
+    pendingExpirationModal()
+  }
+
+  if (licensingInfo.license === 'free') {
     const interval = setInterval(() => {
       if (Studio.getAllEntities().filter((e) => e.__entitySet === 'templates' && !e.__isNew).length > 5) {
         clearInterval(interval)
         trialModal()
-        Studio.extensions['licensing'].options.license = 'trial'
+        licensingInfo.type = licensingInfo.license = 'trial'
         Studio.api.post('/api/licensing/trial', {})
       }
     }, 10000)
   }
 
   Studio.addToolbarComponent((props) => <div
-    className='toolbar-button' onClick={() => window.open('http://jsreport.net/buy', '_blank')}>
-    <div style={{textTransform: 'capitalize'}}><i className='fa fa-gavel' /> {Studio.extensions['licensing'].options.license}</div>
+    className='toolbar-button' onClick={() => licenseInfoModal()}>
+    <div style={{textTransform: 'capitalize'}}><i className='fa fa-gavel' /> {licensingInfo.license} <i className='fa fa-info-circle' /></div>
   </div>, 'settings')
 })
